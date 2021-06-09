835279d0176e6bc6671c7b83bf55d14b27189890
Sammuel Reis <sammugr@hotmail.com>
2021-06-09 19:15:32 -0300

    clears initial code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,16 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    es2021: true,
+    'react-native/react-native': true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-native'],
+  rules: {},
 };
